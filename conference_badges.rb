f518cba9e186b0bb32bc1293975377b21d2786fb
stanford kangethe <stanfordkangethe16@gmail.com>
2022-08-21 16:02:34 +0300

    commit tree 44545cbdd46af1b77244de18a368dbc576a4a6e2

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  names.map {|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(names)
+  names.map do |name|
+    "Hello, #{name}! You'll be assigned to room #{names.find_index(name) + 1}!"
+  end
+end
+
+def printer(names)
+  badges = batch_badge_creator(names)
+  badges.each { |badge| puts badge}
+  
+  rooms = assign_rooms(names)
+  rooms.each { |room| puts room }
+end
+
+# puts batch_badge_creator(['stan', 'abby', 'frank'])
+# puts assign_rooms(['stan', 'abby', 'frank'])
+printer(["Arel", "Carol"]) 
